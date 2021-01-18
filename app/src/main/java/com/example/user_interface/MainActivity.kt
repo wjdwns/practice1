fa9e0ee58211f7b180cc9f76b1433c06918a0b9d
zzeong_program <50354282+wjdwns@users.noreply.github.com>
2021-01-18 21:18:52 +0900

    dialog 추가함

--- app/src/main/java/com/example/user_interface/MainActivity.kt
+++ app/src/main/java/com/example/user_interface/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.user_interface
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    //asdasdasdasdsadasd
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("작성중안 내용을 저장하지 않고 나가시겠습니까?")
+        builder.setPositiveButton("확인", dialogListener)
+        builder.setNegativeButton("취소", null)
+        val dialog = builder.create()
+        dialog.show()
+    }
+    val dialogListener = object : DialogInterface.OnClickListener{
+        override fun onClick(dialog: DialogInterface?, which: Int) {
+            finish()
+        }
+    }
 }
